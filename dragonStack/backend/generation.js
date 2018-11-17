5698e67946d25ed6f86797ba8aa22f9730646dbc
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-17 11:12:12 -0500

    added a generation timeout

--- dragonStack/backend/generation.js
+++ dragonStack/backend/generation.js
@@ -1,3 +1,4 @@
+const Dragon = require('./dragon');
 const { REFRESH_RATE, SECONDS } = require('./config');
 
 const refreshRate = REFRESH_RATE * SECONDS;
@@ -14,4 +15,13 @@ class Generation {
 
 		return new Date(Date.now() + msUntilExpiration);
 	}
+
+	newDragon() {
+		if (Date.now() > this.expiration) {
+			throw new Error(`This generation expired on ${this.expiration}`);
+		}
+		return new Dragon();
+	}
 }
+
+module.exports = Generation;
--- dragonStack/backend/index.js
+++ dragonStack/backend/index.js
@@ -1,21 +1,36 @@
-const Dragon = require('./dragon');
+const Generation = require('./generation');
 
-const fooey = new Dragon({
-	birthdate: new Date(),
-	nickname: 'fooey'
-});
-const baloo = new Dragon({
-	birthdate: new Date(),
-	nickname: 'baloo',
-	traits: [ { traitType: 'backgroundColor', traitValue: 'green' } ]
-});
+const generation = new Generation();
 
-const mimar = new Dragon();
+console.log('generation', generation);
+
+const gooby = generation.newDragon();
+
+console.log('gooby', gooby);
+
 setTimeout(() => {
-	const gooby = new Dragon();
-	console.log('gooby', gooby);
-}, 3000);
+	const mimar = generation.newDragon();
+	console.log('mimar', mimar);
+}, 15000);
 
-console.log('fooey', fooey);
-console.log('baloo', baloo);
-console.log('mimar', mimar);
+// const Dragon = require('./dragon');
+
+// const fooey = new Dragon({
+// 	birthdate: new Date(),
+// 	nickname: 'fooey'
+// });
+// const baloo = new Dragon({
+// 	birthdate: new Date(),
+// 	nickname: 'baloo',
+// 	traits: [ { traitType: 'backgroundColor', traitValue: 'green' } ]
+// });
+
+// const mimar = new Dragon();
+// setTimeout(() => {
+// 	const gooby = new Dragon();
+// 	console.log('gooby', gooby);
+// }, 3000);
+
+// console.log('fooey', fooey);
+// console.log('baloo', baloo);
+// console.log('mimar', mimar);
