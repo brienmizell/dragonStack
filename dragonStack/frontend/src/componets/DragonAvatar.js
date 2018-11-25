ba524bdc0edd04426741ebd56bd2b724e545dd14
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-24 20:38:06 -0500

    reverted back a few commits to find error

--- dragonStack/frontend/src/componets/DragonAvatar.js
+++ dragonStack/frontend/src/componets/DragonAvatar.js
@@ -1,40 +1,5 @@
 import React, { Component } from 'react';
-
-import { skinny, slender, sporty, stocky, patchy, plain, spotted, striped } from '../assets';
-
-const propertyMap = {
-	backgroundColor: {
-		black: '#263238',
-		white: '#CFD8DC',
-		green: '#A5D6A7',
-		blue: '#0277BD'
-	},
-	build: { slender, stocky, sporty, skinny },
-	pattern: { plain, striped, spotted, patchy },
-	size: { small: 100, medium: 140, large: 180, enormous: 220 }
-};
-
 class DragonAvatar extends Component {
-	get DragonImage() {
-		const dragonPropertyMap = {};
-		this.props.dragon.traits.forEach((trait) => {
-			const { traitType, traitValue } = trait;
-
-			dragonPropertyMap[traitType] = propertyMap[traitType][traitValue];
-		});
-
-		const { backgroundColor, build, pattern, size } = dragonPropertyMap;
-
-		const sizing = { width: size, height: size };
-		return (
-			<div className="dragon-avatar-image-wrapper">
-				<div className="dragon-avatar-image-background" style={{ backgroundColor, ...sizing }} />
-				<img src={pattern} className="dragon-avatar-image-pattern" style={{ ...sizing }} />
-				<img src={build} className="dragon-avatar-image" style={{ ...sizing }} />
-			</div>
-		);
-	}
-
 	render() {
 		const { generationId, dragonId, traits } = this.props.dragon;
 		return (
