6719f584e3036234cc52f4111a3deac6bbff78eb
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-27 09:22:48 -0500

    fixing bugs

--- dragonStack/frontend/dist/src.e31bb0bc.js
+++ dragonStack/frontend/dist/src.e31bb0bc.js
@@ -43265,7 +43265,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64413" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65403" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
