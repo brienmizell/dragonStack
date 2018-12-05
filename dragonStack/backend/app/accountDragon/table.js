c996807ebea4e7d09cb897aea57e607a2746e836
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-05 10:22:33 -0500

    store account dragon

---
+++ dragonStack/backend/app/accountDragon/table.js
@@ -0,0 +1,26 @@
+const pool = require("../../databasePool");
+
+class AccountDragonTable {
+  static storeAccountDragon({ accountId, dragonId }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'INSERT INTO accountDragon("accountId", "dragonId") VALUES ($1, $2)',
+        [accountId, dragonId],
+        (error, response) => {
+          if (error) return reject(error);
+
+          resolve();
+        }
+      );
+    });
+  }
+}
+
+AccountDragonTable.storeAccountDragon({
+  accountId: 1,
+  dragonId: 1
+})
+  .then(() => console.log("stored account dragon"))
+  .catch(error => console.error("error", error));
+
+module.exports = AccountDragonTable;
